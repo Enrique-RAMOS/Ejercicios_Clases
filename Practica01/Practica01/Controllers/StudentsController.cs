c3e8ff6c84ff0d931119dba914492f3786e537d9
Enrique-RAMOS <84140676+Enrique-RAMOS@users.noreply.github.com>
2022-04-09 23:30:57 -0600

    New

---
+++ Practica01/Practica01/Controllers/StudentsController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica01.Models;
+using Practica01.Service;
+
+namespace Practica01.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        private IEstudiante Estudiante;
+
+        public StudentsController(IEstudiante estudiante)
+        {
+            Estudiante = estudiante;
+        }
+
+        public IActionResult Delete(int id) {
+            Estudiantes es = new Estudiantes();
+            es.Id = id;
+            Estudiante.DeleteStudents(es);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Load(int id) {
+            Estudiantes es = new Estudiantes();
+            es.Id=id;
+           var listarestudiante= Estudiante.LoadInformation(es);
+
+            return View(listarestudiante);
+        }
+
+        public IActionResult actu(Estudiantes es) {
+
+            Estudiante.UpdateStudents(es);
+            return RedirectToAction("Index");
+
+        }
+
+
+        public IActionResult Guardar() { 
+
+            
+        
+            return View();
+        }
+
+        public IActionResult Save(Estudiantes es) {
+
+            Estudiante.AddStudents(es);
+            return RedirectToAction("Index");
+
+        }
+
+       
+        public IActionResult Index()
+        {
+           
+            var listar = Estudiante.GetAll();
+           
+            return View(listar);
+        }
+        [HttpPost]
+        public IActionResult Insertardata(Estudiantes es)  { 
+
+        return RedirectToAction("Index");
+       // return View("Index");
+        
+        }
+    }
+}
